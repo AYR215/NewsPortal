d28cfc7b30eff0b00697b17094407c0d163f677b
Ayrat <ayr215@mail.ru>
2022-04-05 12:01:19 +0300

    NewsPaper

--- NewsPaper/news/apps.py
+++ NewsPaper/news/apps.py
@@ -8,7 +8,7 @@ class NewsConfig(AppConfig):
 
     # нам надо переопределить метод ready, чтобы при готовности нашего приложения импортировался модуль со всеми функциями обработчиками
     def ready(self):
-        import NewsPaper.news.signals
+        import news.signals
 
 
 #red = redis.Redis(
